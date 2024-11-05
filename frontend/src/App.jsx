1165b70d1718578bb5214f6d642686d60fe4cc00
Pradeep <pradeepubuntuvps@gmail.com>
2024-11-05 14:22:31 +0530

    Add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { Home, About, Contact, MinifyUrl } from "./pages/index";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/minify" element={<MinifyUrl />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/minify" element={<MinifyUrl />} />
+      </Routes>
+    </>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+    const customNavLinkStyle = function ({ isActive }) {
+        return isActive ? "text-white underline underline-offset-4" : "";
+    }
     return (
-        <div>Navbar</div>
+        <nav className="flex justify-between px-4 py-3 bg-blue-700 text-slate-300">
+            <div className="">
+                <h1 className="font-bold text-2xl">Minify URL</h1>
+            </div>
+            <ul className="flex gap-4 text-lg">
+                <li><NavLink className={customNavLinkStyle} to={"/"}>Home</NavLink></li>
+                <li><NavLink className={customNavLinkStyle} to={"/about"}>About</NavLink></li>
+                <li><NavLink className={customNavLinkStyle} to={"/contact"}>Contact</NavLink></li>
+                <li><NavLink className={customNavLinkStyle} to={"https://github.com/PradeepG-07/minify-url.git"} target="_blank">Github</NavLink></li>
+                <li><NavLink className={customNavLinkStyle} to={"/minify"}>Try Now</NavLink></li>
+            </ul>
+        </nav>
     )
 }
 
