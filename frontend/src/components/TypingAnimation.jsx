72821956f33a33ffb3bd61f21dcc2695d4819dc7
Pradeep <pradeepubuntuvps@gmail.com>
2024-11-10 23:06:06 +0530

    Add Home Page

---
+++ frontend/src/components/TypingAnimation.jsx
@@ -0,0 +1,34 @@
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
+
+export default function TypingAnimation({ strings = ["Put your string here"] }) {
+    // Create Ref element.
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: strings, // Strings to display
+            startDelay: 100,
+            typeSpeed: 40,
+            backSpeed: 10,
+            backDelay: 200,
+            smartBackspace: true,
+            loop: true,
+            showCursor: true,
+            cursorChar: "|"
+        });
+
+        // Destropying
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+    return (
+        <span ref={el}>Hello Developers</span>
+    );
+}
+TypingAnimation.propTypes = {
+    strings: PropTypes.array,
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,31 @@
-
+import { Link } from "react-router-dom";
+import TypingAnimation from "../components/TypingAnimation";
 const Home = () => {
+    const animatedLines = [
+        "Shrink your links, amplify your reach!",
+        "Make your URLs short and sweet!",
+        "Cut the clutter—keep it short!",
+        "The perfect link, now bite-sized.",
+        "Your long links deserve a quick makeover.",
+        "Brevity is the soul of a great URL!",
+        "Simplify your links, boost your clicks.",
+        "Make your links easy to remember and share!",
+        "Turning long URLs into short stories.",
+        "Just a few clicks to a clean link!"
+    ]
     return (
-        <div>Home</div>
+        <div className="min-h-[84vh] items-center justify-center flex flex-col gap-2 md:gap-7 pt-3" style={{ backgroundColor: "#09709236" }}>
+
+            <div className="md:w-1/2 min-h-24">
+                <h1 className="text-5xl font-extrabold text-wrap text-center">
+                    <span className="text-gray-800"><TypingAnimation strings={animatedLines} /></span>
+                </h1>
+            </div>
+            <div className="flex flex-col gap-2 md:gap-7 justify-center">
+                <p className="text-2xl font-bold text-gray">What are you waiting for start using short urls <span className="text-pink-500">for free..</span></p>
+                <Link className="w-fit mx-auto px-4 text-xl py-2 rounded bg-amber-600 hover:bg-amber-700 transition-all text-white" to={"/minify"}>Get Started</Link>
+            </div>
+        </div>
     )
 }
 
