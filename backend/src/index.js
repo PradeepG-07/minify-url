0f8baf219e8c057d8b8336a1e3caf02700b24fe8
pradeep <pradeepubuntuvps@gmail.com>
2024-11-21 08:52:30 +0530

    Update MongoDB connection and add DB_NAME to environment schema; modify Vercel configuration for CORS and rewrites

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import cleanedEnv from "./utils/cleanedEnv.js";
 import mongoose from "mongoose";
 import app from "./app.js";
 
-mongoose.connect(cleanedEnv.MONGODB_URL)
+mongoose.connect(cleanedEnv.MONGODB_URL,{dbName: cleanedEnv.DB_NAME})
     .then((res) => {
         app.listen(cleanedEnv.PORT, () => {
             console.log("Server started at port ", process.env.PORT);
--- backend/src/utils/zodSchemas.js
+++ backend/src/utils/zodSchemas.js
@@ -4,6 +4,7 @@ export const envSchema = z.object({
     PORT: z.string({ message: "PORT is required." }),
     FRONTEND_URL: z.string({ message: "FRONTEND_URL is required." }),
     MONGODB_URL: z.string({ message: "MONGODB_URL is required." }),
+    DB_NAME: z.string({ message: "DB_NAME is required." }),
 });
 
 export const urlBodyRequestSchema = z.object({
